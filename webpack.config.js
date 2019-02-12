89e8a0dda6a489f827701ce9cb77e2298ba894ad
Will Amaral <willian.amaral@outlook.com.br>
2019-02-12 15:22:39 -0200

    Added dependencies and initial config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+"use strict";
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: './client/src/index.js',
+	module: {
+		rules: [
+			{	exclude: /node_modules/, test: /\.js$/, use: 'babel-loader' },
+			{ test: /\.(png|jpg|gif|ico)$/, use: {loader: 'file-loader'}},
+			{ 
+				test: /\.(sass|scss|css)$/, 
+				use: [
+					process.env.NODE_ENV != 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './client/public/index.html'
+		}),
+		new Dotenv(),
+		new MiniCssExtractPlugin()
+	],
+	devServer: {
+		port: 3000
+	}
+};
