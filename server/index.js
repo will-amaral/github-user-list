574ea70c0dedf8eaac089cca3b898a2aa349139e
Will Amaral <willian.amaral@outlook.com.br>
2019-02-12 19:22:36 -0200

    Created proxy server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,48 @@
-console.log("Servidor funcionando");
+const octokit = require('@octokit/rest')();
+const app = require('express')();
+
+const getUsers = (since) => {
+	let data = octokit.users.list({since});
+	return data;
+};
+
+const getUserDetails = (username) => {
+	let data = octokit.users.getByUsername({username});
+	return data;
+};
+
+const getUserRepos = (username) => {
+	let data = octokit.repos.listForUser({username});
+	return data;
+}
+
+app.get('/api/users', async (req, res) => {
+	try {
+		const users = await getUsers(req.query.since);
+		res.json(users);
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+app.get('/api/users/:username/details', async (req, res) => {
+	try {
+		const user = await getUserDetails(req.params.username);
+		res.json(user);
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+app.get('/api/users/:username/repos', async (req, res) => {
+	try {
+		const repos = await getUserRepos(req.params.username);
+		res.json(repos);
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+app.listen(3001, () => {
+	console.log('Servidor na porta 3001')
+});
